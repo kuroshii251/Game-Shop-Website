dd06aa8c76819308a834deda2014d85fb95febe6
sncthyz <andikacahyarahman123@gmail.com>
2026-06-12 18:44:11 +0700

    done_2

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -32,10 +32,10 @@ function GameSwiper({ games }) {
         modifier: 1,
         slideShadows: true,
       }}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
+      // autoplay={{
+      //   delay: 5500,
+      //   disableOnInteraction: false,
+      // }}
       modules={[EffectCoverflow, Navigation, Autoplay]}
       className="gameSwiper"
     >
